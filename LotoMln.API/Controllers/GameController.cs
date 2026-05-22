f2e80aaab99b3ed2efad591a2a7c684bc41468e7
LongTran2003 <quylongtranngoc07@gmail.com>
2026-05-22 16:49:35 +0700

    feat: fix name #2

--- LotoMln.API/Controllers/GameController.cs
+++ LotoMln.API/Controllers/GameController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace LotoMln.API.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/Rooms/{code}/game")]
 public class GameController(IGameEngineService engine) : ControllerBase
 {
     [HttpGet("state")]
